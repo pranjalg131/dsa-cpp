bee833e5dd458043c076d7a9d48c4624fa5aa98d
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-10-27 00:27:05 +0530

    Create sieve.cpp

---
+++ random/sieve.cpp
@@ -0,0 +1,12 @@
+vector<int> sieve(int n)
+{
+    int*arr = new int[n + 1]();
+    vector<int> vect;
+    for (int i = 2; i <= n; i++)
+        if (arr[i] == 0) {
+            vect.push_back(i);
+            for (int j = i * i; j <= n; j += i)
+                arr[j] = 1;
+        }
+    return vect;
+}
